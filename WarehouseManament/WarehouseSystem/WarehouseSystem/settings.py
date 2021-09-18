ef87159fa71ac2c1d4a8f2e00fdc471b127b8a8c
phuocle1310 <phuoc.lh1310@gmail.com>
2021-09-18 15:12:44 +0700

    Add Bearer vào swagger

--- WarehouseManament/WarehouseSystem/WarehouseSystem/settings.py
+++ WarehouseManament/WarehouseSystem/WarehouseSystem/settings.py
@@ -48,6 +48,27 @@ INSTALLED_APPS = [
 ]
 
 
+SWAGGER_SETTINGS = {
+    'SHOW_REQUEST_HEADERS': True,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
+    'SUPPORTED_SUBMIT_METHODS': [
+        'get',
+        'post',
+        'put',
+        'delete',
+        'patch'
+    ],
+}
+
+
 #auth
 OAUTH2_INFO = {
     "client_id": "Hn9VGL0a2J6Ae2zljKWLeFYqy6cqXOXKJCrEorut",
