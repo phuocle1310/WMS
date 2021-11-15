f93177263d69709911abbe728093a660fc508514
vothikimyen108 <vothikinyen2k@gmail.com>
2021-11-15 22:21:43 +0700

    update file pdf

--- wms/src/store/index.js
+++ wms/src/store/index.js
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import language from "./language";
 import userSlice from "./userSlice";
 import soSlice from "./soSlice";
-// import productSlice from "./productSlice";
 import poSlice from "./poSlice";
 const store = configureStore({
   reducer: {
     currentLanguage: language.reducer,
     user: userSlice.reducer,
-    // product: productSlice.reducer,
     po: poSlice.reducer,
     so: soSlice.reducer,
   },
